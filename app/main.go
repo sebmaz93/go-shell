346c1349d742995367cce8a6f2f4b8dacd3ffab1
sebmaz <seb.maz93@icloud.com>
2025-12-09 02:12:32 +0400

    add read std in

--- app/main.go
+++ app/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatal("something went wrong")
+	}
+	command = strings.TrimSpace(command)
+	fmt.Printf("%s: command not found\n", command)
 }
